b4e7eb81a667d8b55380bd4e2e14e0766ab144a5
Yuriy Miroshnyk <y.miroshnyk@gmail.com>
2014-10-12 12:08:27 +0300

    Sketch for TaskSharedObject.

---
+++ src/asio_ext/asio_ext_task_shared_object.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+
+template <typename T>
+class TaskSharedObject
+{
+	TaskSharedMutex mutex_;
+	std::unique_ptr<T> object_;
+
+public:
+	TaskSharedObject(std::unique_ptr<T> object);
+	// ждём, пока не закончатся таски, при этом не принимаем новые - возвращаем false
+	~TaskSharedObject();
+
+	bool postShared(TaskHandlerP task, boost::function<void (TaskHandlerP, const T&)>) const;
+	bool post(TaskHandlerP task, boost::function<void (TaskHandlerP, T&)>);
+	bool postDelete(TaskHandlerP task, boost::function<void (TaskHandlerP)>);
+
+	operator bool () const;
+};
